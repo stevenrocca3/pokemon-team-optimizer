eb74dcb8d5a523e2e620579a7044424a3c29b03a
Yessir30000 <authorofzeke@gmail.com>
2026-08-21 16:14:43 -0400

    Initial scaffold: React+TS+Tailwind frontend, Spring Boot+H2 backend

---
+++ pokemon-optimizer-backend/src/main/java/com/pokemonoptimizer/pokemon_optimizer_backend/PokemonOptimizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonoptimizer.pokemon_optimizer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonOptimizerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonOptimizerBackendApplication.class, args);
+	}
+
+}
---
+++ pokemon-optimizer-backend/src/test/java/com/pokemonoptimizer/pokemon_optimizer_backend/PokemonOptimizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonoptimizer.pokemon_optimizer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonOptimizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
